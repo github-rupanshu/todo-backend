b016fefe272fcdeb1f74ccb044f955264f458386
Rupanshu <mail2rupanshu@gmail.com>
2021-06-01 17:29:17 +0530

    added message for ping on baseUrl/ping

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 //const homeController = require('../controllers/home_controller');
 
-console.log('router loaded');
+console.log("router loaded");
 
 //route for home page
 //router.get('/',homeController.home);
@@ -11,7 +11,12 @@ console.log('router loaded');
 //route for todo
 //router.use('/todo',require('./todo'));
 
-router.use('/api', require('./api'));
+router.use("/api", require("./api"));
 
+router.get("/ping", (req, res) => {
+  return res.status(200).json({
+    msg: "Pong",
+  });
+});
 
 module.exports = router;
